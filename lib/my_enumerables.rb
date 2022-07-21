d841849c159c78a5c73b50c7335adc1fc981c411
Red <crabmarket56@gmail.com>
2022-07-21 19:29:37 -0400

    Add my_any? method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -36,6 +36,21 @@ module Enumerable
     i += 1
     end
   end
+
+  def my_any?
+    i = 0
+    while i < self.length
+      true_or_false = yield(self[i])
+      if true_or_false == true
+        return true
+        break
+      elsif i == self.length - 1
+        return false
+      end
+      i += 1
+    end
+  end
+
 end
 
 # You will first have to define my_each
